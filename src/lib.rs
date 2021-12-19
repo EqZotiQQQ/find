53873d7d4628c07dc156dea1f450bceaf09b384b
Mikhail Fedyakov <mf_52@mail.ru>
2021-12-19 20:20:33 +0300

    Some impovements?

--- src/lib.rs
+++ src/lib.rs
@@ -26,21 +26,73 @@ impl Config {
 //      return Ok(Path::new(&config.path))
 // }
 
-pub fn search_depth(path: &Path, results: &mut Vec<String>, config:&Config) {
-    // println!("{}", path.to_str().unwrap().red());
-    for i in path.read_dir().unwrap() {
-        // if i.as_ref().unwrap().path().to_str().unwrap().contains(&config.target) {
-        // }
-        if i.as_ref().unwrap().file_name().to_str().unwrap().ends_with(&config.target) {
-            results.push(i.as_ref().unwrap().path().to_str().unwrap().parse().unwrap());
+pub fn search_depth(path: &Path, results: &mut Vec<String>, config: &Config) {
+
+    // path.read_dir().unwrap().into_iter().map(|item| println!("item: {:?}", item));
+
+    // for_each terminate operation
+    // map is non-terminate operator
+    // path.read_dir().unwrap().into_iter().for_each(|item| println!("item: {:?}", item));
+    // path.read_dir().unwrap().into_iter().for_each(|item| println!("{}", item.unwrap().file_name().to_str().unwrap())); // prints file names
+    // path.read_dir().unwrap().into_iter().for_each(|item| if let x= item.unwrap().file_type().unwrap() {
+    //     x.is_dir().then(|| { println!("dir {:?}", x) });
+    //     x.is_file().then(|| { println!("file {:?}", x) });
+    // });
+
+    // path.read_dir().unwrap().into_iter().for_each(|item| if let x= item.unwrap() {
+    //     x.file_type().unwrap().is_file().then(|| { println!("file {:?}", x) });
+    //     x.file_type().unwrap().is_dir().then(|| { search_depth(x.path().as_path(), results, config) });
+    // });
+
+    match path.read_dir() {
+        Ok(red_dir) => {
+            red_dir.for_each(|file| {
+                match file {
+                    Ok(dir_entry) => {
+                        match dir_entry.file_name().to_str() {
+                            None => {
+                                println!("No such file?");
+                            }
+                            Some(s) => {
+                                dir_entry.path().is_dir().then(|| {
+                                    search_depth(dir_entry.path().as_path(), results, config);
+                                });
+                                s.contains(&config.target).then(|| {
+                                    results.push(String::from(dir_entry.path().to_str().unwrap()));
+                                });
+                            }
+                        }
+                    }
+                    Err(_) => {}
+                }
+            })
         }
-        if i.as_ref().unwrap().path().is_dir() {
-            search_depth(i.unwrap().path().as_path(), results, config);
-        } else {
-            // its a file
-            // println!("{}", i.unwrap().path().to_str().unwrap().green());
-        }
+        Err(red_dir_err) => { println!("Failed to read directory: \
+        The provided path doesn’t exist.\
+        The process lacks permissions to view the contents.\
+        The path points at a non-directory file. {}", red_dir_err); }
     }
+    // path.read_dir().unwrap().into_iter().for_each(|item| {
+    //
+    // });
+
+    // path.map(|item| println!("item: {:?}", item));
+
+    // for i in path.read_dir().unwrap() {
+    //
+    // }
+    // println!("Current root: {}", path.to_str().unwrap().red());
+    // for i in path.read_dir().unwrap() {
+    //     if i.as_ref().unwrap().file_name().to_str().unwrap().ends_with(&config.target) {
+    //          results.push(i.as_ref().unwrap().path().to_str().unwrap().parse().unwrap());
+    //     }
+    //     if i.as_ref().unwrap().path().is_dir() {
+    //         search_depth(i.unwrap().path().as_path(), results, config);
+    //     } else {
+    //         // its a file
+    //         // println!("{}", i.unwrap().path().to_str().unwrap().green());
+    //     }
+    // }
 }
 
 
